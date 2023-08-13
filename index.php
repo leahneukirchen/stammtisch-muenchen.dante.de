498042ce6761921bdfef0cd38d085d630869e0ed
leahneukirchen <leahneukirchen@users.noreply.github.com>
2023-08-13 14:16:19 +0000

    deploy: 20db621a7081af1a49ab088cb6ef630723441259

---
+++ index.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="de-de">
+
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>TeX-Stammtisch in München</title>
+  <style>
+body {
+  background-color: #f7f7f7;
+  color: #333333;
+  font-family: "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+  font-size: 16px;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+a:link, a:visited {
+  color: #424242;
+}
+
+a:hover, a:active {
+  color: #555555;
+}
+
+h1 {
+  font-size: 1.5em;
+  padding: 1.5em 0 0 0;
+}
+
+h2 {
+  font-size: 1.25em;
+}
+
+.main {
+  background: #ffffff;
+  -webkit-box-shadow: 0 10px 20px 0 rgba(236, 236, 236, 0.86);
+          box-shadow: 0 10px 20px 0 rgba(236, 236, 236, 0.86);
+  width: 800px;
+  margin: 60px auto;
+
+}
+
+.content {
+  width: 740px;
+  height: auto;
+  margin: 0 auto;
+  padding: 0 0 20px 0;
+}
+
+.headerimg {
+  background-image: url(./header.jpeg);
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: 20%;
+  width: 100%;
+  height: 130px;
+  margin: 25px 0 1em 0;
+}
+
+.updated {
+  text-align: right;
+}
+
+  </style>
+</head>
+
+<body>
+  <div class="main">
+    <div class="content">
+      <h1>TeX-Stammtisch in München</h1>
+      <div class="headerimg"></div>
+
+  <div class="next">
+
+<h2>Unsere Treffen</h2>
+
+<p>
+Wir treffen uns alle zwei Monate in der ersten Woche der geradzahligen Monate,
+jeweils um 19 Uhr.
+Dabei wechseln wir den Wochentag zyklisch von Dienstag bis Donnerstag.
+Falls der Termin wirklich einmal auf einen Feiertag trifft oder in
+allgemeine Urlaubszeit fällt, weichen wir auf die zweite Woche aus.
+</p>
+
+<h2>Nächster Termin</h2>
+
+
+<p>
+Am Dienstag, den 3. Oktober 2023 ist wieder TeX-Stammtisch in München.
+</p>
+
+<p>
+Wir treffen uns um 19:00 Uhr hier:
+</p>
+
+<blockquote>
+»Schiller Bräu«<br>
+Schillerstraße 23<br>
+80336 München<br>
+Haltestelle »Hauptbahnhof«<br>
+<a href="https://www.schiller-braeu.de/de/">https://www.schiller-braeu.de/de/</a><br>
+</blockquote>
+
+<p>
+Um ausreichend Plätze reservieren zu können, wird eine Rückmeldung unter
+<a href="https://nuudel.digitalcourage.de/HdiYQUHkVggQ7cnk">https://nuudel.digitalcourage.de/HdiYQUHkVggQ7cnk</a>
+erbeten.
+</p>
+
+<p>
+Alle, die sich für TeX, LaTeX und guten Schriftsatz interessieren, sind
+herzlich eingeladen.
+</p>
+
+
+<h2>Terminvorschau</h2>
+
+Außer dem oben angegebenen nächsten Termin sind die bislang geplanten weiteren
+Termine:
+
+<ul>
+<li>Mittwoch, 2023-12-06</li>
+</ul>
+
+
+
+</div>
+
+  <div class="past">
+
+<h2>Vergangene Termine</h2>
+
+<ul>
+<li>Donnerstag, 2023-07-27 im Schiller Bräu</li>
+<li>Donnerstag, 2023-06-01 im Schiller Bräu</li>
+<li>Mittwoch, 2023-04-05 im Schiller Bräu</li>
+<li>Dienstag, 2023-02-07 im Schiller Bräu</li>
+<li>Donnerstag, 2022-12-08 im Rumpler</li>
+<li>Mittwoch, 2022-10-05 im Schiller Bräu</li>
+<li>Montag, 2022-07-11 im Rumpler</li>
+<li>Mittwoch, 2022-06-15 im Rumpler</li>
+</ul>
+
+  </div>
+
+  <div class="about">
+
+    <h2>Über den Münchner TeX-Stammtisch</h2>
+
+<p>
+Unser Stammtisch wurde im Oktober 1998 von Michael Niedermair gegründet
+und wird derzeit von <a href="mailto:leah@vuxu.org">Leah Neukirchen</a>
+organisiert.
+Er ist ein offener Treff für alle, die an Diskussion und Information
+über TeX und sein Umfeld und über Schriftsatz, Typografie und
+Druckereiwesen in irgendeiner Weise interessiert sind. Willkommen sind
+vor allem auch Einsteiger auf der Suche nach Tipps, Erfahrungen und
+Ratschlägen zum Einsatz von LaTeX und anderen TeX-Formaten.
+</p>
+
+  </div>
+
+    <h2>Mailingliste</h2>
+
+<p>
+Für die Stammtischteilnehmer und sonstige Interessierte gibt es auf dem Server
+von <a href="https://dante.de">Dante e. V.</a>
+eine geschlossene Mailingliste. Es gibt dort Ankündigungen des
+jeweils nächsten Stammtischtermins und diverse Diskussionen unter den
+Mitgliedern. Wer in diese Liste aufgenommen werden möchte,
+kann sich <a href="https://lists.dante.de/mailman/listinfo/stammtisch-muenchen">online anmelden</a>.
+</p>
+
+
+<p class="updated">
+<em>Letzte Aktualisierung: 2023-08-13 14:16</em>
+</p>
+  </div>
+  </div>
+</body>
