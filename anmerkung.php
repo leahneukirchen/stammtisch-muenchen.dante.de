fc4b08af2bc8817118029d7e38c2154e85f4ad55
Leah Neukirchen <leah@vuxu.org>
2024-02-09 20:13:50 +0100

    apr 2024

--- anmerkung.php
+++
@@ -1,7 +0,0 @@
-Dieses Mal beginnen wir schon um <b>18:00 Uhr</b>, und es wird in der
-<i>Technischen Universität München, Gebäude N5</i> einen Vorträg geben:
-<ul>
-<li><a href="https://typst.app">Typst</a> als LaTeX Alternative &ndash; erste Schritte</li>
-</ul>
-
-Danach gemeinsames Nachtreffen im <a href="http://www.tresznjewski.com/">Tresznjewski</a> (fußläufig).
